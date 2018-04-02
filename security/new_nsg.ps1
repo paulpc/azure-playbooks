cffee99db925ebed29569cdee502918b2da9bd8e
Paul Poputa-Clean <praetor44@gmail.com>
2018-04-02 09:17:21 -0500

    Updated new_nsg.ps1 in Azure Automation.

--- security/new_nsg.ps1
+++ security/new_nsg.ps1
@@ -47,70 +47,115 @@ if($Data.operationName -match "MICROSOFT.NETWORK/NETWORKSECURITYGROUPS/WRITE")
         -CertificateThumbprint $ServicePrincipalConnection.CertificateThumbprint | Write-Verbose
     
     # selecting the apropriate subscription
-    write-host $Data.resourceUri
-    $subby = ($Data.resourceUri -split "/")[2]                                                                                                                                                                  
-    Select-AzureRmSubscription $subby
+    $Data.resourceUri | Write-Verbose
+    $subby = $Data.subscriptionId
+
+    $subby | Write-Verbose                                                                                                                                                         
+    #Set-AzureRMContext  -SubscriptionId $subby
     $rg = ($Data.resourceUri -split "/")[4]                                                                                                                                                                     
     $nsg_name = ($Data.resourceUri -split "/")[8] 
     # Set subscription to work against
-    Set-AzureRmContext -SubscriptionID $ServicePrincipalConnection.SubscriptionId | Write-Verbose
+    Set-AzureRmContext -SubscriptionId $subby | Write-Verbose
 
     $nsg = Get-AzureRmNetworkSecurityGroup  -ResourceGroupName $rg -Name $nsg_name
-    foreach ($networkwatcher in Get-AzurermNetworkWatcher  -ResourceGroupName NetworkWatcherRg) {
-        if ($nsg.Location -eq $networkwatcher.Location) {
-            $fl_status = Get-AzureRmNetworkWatcherFlowLogStatus -NetworkWatcher $NW -TargetResourceId $nsg.Id
-            if ( ! ($fl_status.Enabled)) {
 
-                # this is where we enable the logging
-                $logging = "not logging"
-                #first of all getting the blob account
-                $found=$false
-                $blob_name_start=($subby -split "-")[0]
-
-                foreach ($store in Get-AzureRmStorageAccount -ResourceGroupName NetworkWatcherRG) {
-                    # looking for blobs in the NetworkWatcherRG group and making sure they are the right odd-named-ones
-                    if ($store.Location -eq $nsg.Location -And $store.name.StartsWtih($blob_name_start)) {
-                        # that match the location of the NSG
-                        $found=$store
-                    }
-                }
-
-                # if we found one, we can set it
-                if ($found) {
-                    Set-AzureRmNetworkWatcherConfigFlowLog -NetworkWatcher $networkwatcher -TargetResourceId $nsg.Id -EnableFlowLog $true -StorageAccountId $found.Id
-                    $logging = "logging to $($found.Id)"
-                }
-
-                if (!([string]::IsNullOrEmpty($ChannelURL)))
-                    {
-                        $TargetURL = "https://portal.azure.com/#resource" + $Data.resourceUri + "/overview"   
-                        
-                        $Body = ConvertTo-Json -Depth 4 @{
-                        title = 'NSG Creation notification' 
-                        text = 'NSG was created, but is not getting logged.'
-                        sections = @(
-                            @{
-                            activityTitle = 'Azure NSG'
-                            activitySubtitle = 'NSG ' + $nsg.Name + ' has been created'
-                            activityText = 'NSG ' + $subby + ' and resource group ' + $nsg.ResourceGroupName
-                            activityImage = 'https://azure.microsoft.com/svghandler/automation/'
-                            }
-                        )
-                        potentialAction = @(@{
-                            '@context' = 'http://schema.org'
-                            '@type' = 'ViewAction'
-                            name = 'Click here to manage the NSG'
-                            target = @($TargetURL)
-                            })
-                        }
-                        
-                        # call Teams webhook
-                        Invoke-RestMethod -Method "Post" -Uri $ChannelURL -Body $Body | Write-Verbose
-                    }
+    # If we're able to get details about the NSG, let's do something about it. Otherwise just complain about it.
+    if ($nsg) {
+        # let's look at the rules:
+        $jit_protection=@()
+        $rule_mesage=""
+        foreach ($securityRule in $nsg.SecurityRules) {
+            if ($securityRule.Name.startsWith("SecurityCenter-JITRule") -And $securityRule.Access -eq "Deny"){
+                $jit_protection+=$securityRule.DestinationPortRange
+            } elseif ($securityRule.Name.startsWith("default-allow") ) {
+                foreach ($port in $securityRule.DestinationPortRange) {
+                    if ( ! ($port -in $jit_protection) -And "*" -in $securityRule.SourceAddressPrefix) {
+                        $rule_mesage+=("found insecure shit - " + $securityRule.Name + " - " + $nsg.Name + "; ")
+                    } 
                 }
             }
         }
+        foreach ($networkwatcher in Get-AzurermNetworkWatcher  -ResourceGroupName NetworkWatcherRg) {
+            if ($nsg.Location -eq $networkwatcher.Location) {
+                $fl_status = Get-AzureRmNetworkWatcherFlowLogStatus -NetworkWatcher $networkwatcher -TargetResourceId $nsg.Id
+                if ( ! ($fl_status.Enabled)) {
+
+                    # this is where we enable the logging
+                    $logging = "not logging"
+                    #first of all getting the blob account
+                    $found=$false
+                    $blob_name_start=($subby -split "-")[0]
+
+                    foreach ($store in Get-AzureRmStorageAccount -ResourceGroupName NetworkWatcherRG) {
+                        # looking for blobs in the NetworkWatcherRG group and making sure they are the right odd-named-ones
+                        if ($store.Location -eq $nsg.Location -And $store.storageaccountname.StartsWith($blob_name_start)) {
+                            # that match the location of the NSG
+                            #  -And $store.name.StartsWtih($blob_name_start)
+                            $found=$store
+                        }
+                    }
+
+                    # if we found one, we can set it
+                    if ($found) {
+                        Set-AzureRmNetworkWatcherConfigFlowLog -NetworkWatcher $networkwatcher -TargetResourceId $nsg.Id -EnableFlowLog $true -StorageAccountId $found.Id
+                        $logging = "logging to $($found.Id)"
+                    }
+
+                    if (!([string]::IsNullOrEmpty($ChannelURL)))
+                        {
+                            $TargetURL = "https://portal.azure.com/#resource" + $Data.resourceUri + "/overview"   
+                            
+                            $Body = ConvertTo-Json -Depth 4 @{
+                            title = 'NSG Creation notification' 
+                            text = 'NSG was created, '+ $logging
+                            sections = @(
+                                @{
+                                activityTitle = 'Azure NSG'
+                                activitySubtitle = 'NSG ' + $nsg.Name + ' has been created.'
+                                activityText = 'NSG ' + $subby + ' and resource group ' + $nsg.ResourceGroupName + "; " + $rule_mesage
+                                activityImage = 'https://azure.microsoft.com/svghandler/automation/'
+                                }
+                            )
+                            potentialAction = @(@{
+                                '@context' = 'http://schema.org'
+                                '@type' = 'ViewAction'
+                                name = 'Click here to manage the NSG'
+                                target = @($TargetURL)
+                                })
+                            }
+                            
+                            # call Teams webhook
+                            Invoke-RestMethod -Method "Post" -Uri $ChannelURL -Body $Body | Write-Verbose
+                        }
+                    }
+                }
+            }
+    } else {
+        if (!([string]::IsNullOrEmpty($ChannelURL)))
+        {
+            $TargetURL = "https://portal.azure.com/#resource" + $Data.resourceUri + "/overview"   
+            
+            $Body = ConvertTo-Json -Depth 4 @{
+            title = 'NSG Creation notification' 
+            text = 'NSG was created, but I am unable to get many details on it'
+            sections = @(
+                @{
+                activityTitle = 'Azure NSG'
+                activitySubtitle = 'NSG has been created'
+                activityText = 'NSG has been created in the following subscription ' + $subby + ' but I cannot do anything about it'
+                activityImage = 'https://azure.microsoft.com/svghandler/automation/'
+                }
+            )
+            potentialAction = @(@{
+                '@context' = 'http://schema.org'
+                '@type' = 'ViewAction'
+                name = 'Click here to manage the NSG'
+                target = @($TargetURL)
+                })
+            }
+            
+            # call Teams webhook
+            Invoke-RestMethod -Method "Post" -Uri $ChannelURL -Body $Body | Write-Verbose
+        }
     }
-
-
- 
+    }
